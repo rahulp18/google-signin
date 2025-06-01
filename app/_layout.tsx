5acb64d13c108c473c67eba661dd4ad236a4f02d
rahulp18 <pradhanrahul3346@gmail.com>
2025-06-01 11:51:09 +0530

    Add normal google signin

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-
 import "react-native-reanimated";
 
 export default function RootLayout() {
@@ -12,11 +12,10 @@ export default function RootLayout() {
     // Async font loading only occurs in development.
     return null;
   }
-
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-  );
+  GoogleSignin.configure({
+    webClientId:
+      "430327061413-8gkj2cdajfj01f14rj6ulpvltlm5g74o.apps.googleusercontent.com",
+    offlineAccess: false,
+  });
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,51 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+import React, { useEffect } from "react";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 
+WebBrowser.maybeCompleteAuthSession();
 const Home = () => {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:
+      "430327061413-gavm8rqcbepe44aq017513gga334fm51.apps.googleusercontent.com",
+    webClientId:
+      "430327061413-8gkj2cdajfj01f14rj6ulpvltlm5g74o.apps.googleusercontent.com",
+  });
+
+  const signIn = async () => {
+    try {
+      promptAsync();
+
+      console.log("User Info:", response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    if (response) {
+      console.log(response);
+    }
+  }, [response]);
+  const handleSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      console.log({ response });
+      Alert.alert("Success", JSON.stringify(response, null, 2));
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <View>
       <Text>Home</Text>
+      <TouchableOpacity onPress={signIn}>
+        <Text>Google Sign In With Web</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleSignIn}>
+        <Text>Google Sign In With Normal</Text>
+      </TouchableOpacity>
     </View>
   );
 };
